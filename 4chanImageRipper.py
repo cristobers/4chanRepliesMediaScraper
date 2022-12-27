4fef8a78fd12161fd5e14146869e0bdae42bb5a1
cristober <72763707+cristobers@users.noreply.github.com>
2022-12-27 15:20:58 +0000

    Update 4chanImageRipper.py
    program will now recheck a thread if its been downloaded previously and download items which weren't downloaded the first time around

--- 4chanImageRipper.py
+++ 4chanImageRipper.py
@@ -1,30 +1,40 @@
-# ///////////////////////////////////////////
-# grabs all of the images from a 4chan thread
-# provide the thread you want to rip as the second argument:
+#  ///////////////////////////////////////////
+#  grabs all of the images from a 4chan thread
+#  provide the thread you want to rip as the second argument:
 # `python3 4chanImageRipper.py https://boards.4channel.org/g/thread/76759434`
-# ///////////////////////////////////////////
+#  ///////////////////////////////////////////
 import sys, os
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
+updateFromThread = False
 
 try:
 	html = urlopen(sys.argv[1])
 except Exception as e:
 	print(e)
+
 else:
 	bs4 = BeautifulSoup(html.read(), 'html.parser') 
 	postImage = bs4.findAll("a", {"class":"fileThumb"})
 	length = len(postImage)
 	imgDir = sys.argv[1].split('/')[-1]
-	
-	print(f"making dir: {imgDir} and grabbing {length} images...")
-	os.mkdir(imgDir)
+	print(f"making dir {imgDir}/ and grabbing {length} replies...")
+
+	try:
+		os.mkdir(imgDir)
+	except FileExistsError:
+		print(f"A folder with the same name as the thread already exists, grabbing media that haven't been downloaded yet.")
+		updateFromThread = True
 
 	for i, image in enumerate(postImage):
 		try:
-			imageName = image['href'].split('/')[-1]
-			print(f"Grabbing: {imageName} \t {i+1}/{length}")
-			urlretrieve(f"https://{str(image['href'].split('//')[-1])}", f"{imgDir}/{imageName}")
+			threadMedia = image['href'].split('/')[-1]
+			if updateFromThread:
+				if threadMedia in os.listdir(imgDir):
+					print(f"Already downloaded {threadMedia}, skipping...")
+					continue
+			print(f"Grabbing: {threadMedia} \t {i+1}/{length}")
+			urlretrieve(f"https://{str(image['href'].split('//')[-1])}", f"{imgDir}/{threadMedia}")
 		except Exception as e:
 			print(e)
