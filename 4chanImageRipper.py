c8be7ca4d2f3948e351d8441ff73b301d21fa901
cristobers <72763707+cristobers@users.noreply.github.com>
2023-06-14 19:10:56 +0100

    Re-written whole script to use asyncio, runs ~5 seconds faster than it did before, improvements can still be made.

--- 4chanImageRipper.py
+++ 4chanImageRipper.py
@@ -1,40 +1,60 @@
-#  ///////////////////////////////////////////
-#  grabs all of the images from a 4chan thread
-#  provide the thread you want to rip as the second argument:
-#  python3 4chanImageRipper.py https://boards.4channel.org/g/thread/76759434
-#  ///////////////////////////////////////////
-import sys, os
-from urllib.request import urlopen, urlretrieve
+import sys, os, asyncio, aiofiles, aiohttp
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-updateFromThread = False
+if len(sys.argv) < 2:
+	print("usage: 4chanImageRipper.py <thread URL>")
+	exit(1)
 
-try:
-	html = urlopen(sys.argv[1])
-except Exception as e:
-	print(e)
+update_from_thread = False
 
-else:
-	bs4 = BeautifulSoup(html.read(), 'html.parser') 
-	postImage = bs4.findAll("a", {"class":"fileThumb"})
-	length = len(postImage)
-	imgDir = sys.argv[1].split('/')[-1]
-	print(f"making dir {imgDir}/ and grabbing {length} replies...")
+async def grab_media(url, path):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url) as resp:
+			if resp.status == 200:
+				f = await aiofiles.open(path, mode='wb')
+				await f.write(await resp.read())
+				await f.close()
 
-	try:
-		os.mkdir(imgDir)
-	except FileExistsError:
-		print(f"A folder with the same name as the thread already exists, grabbing media that haven't been downloaded yet.")
-		updateFromThread = True
-
-	for i, image in enumerate(postImage):
+async def parse_media_links(post_image, img_dir):
+	for _, image in enumerate(post_image):
 		try:
-			threadMedia = image['href'].split('/')[-1]
-			if updateFromThread:
-				if threadMedia in os.listdir(imgDir):
-					print(f"Already downloaded {threadMedia}, skipping...")
+			href_element = image['href']
+			thread_media = href_element.split('/')[-1]
+			if update_from_thread:
+				if thread_media in os.listdir(img_dir):
+					print(f"Already downloaded {thread_media}, skipping...")
 					continue
-			print(f"Grabbing: {threadMedia} \t {i+1}/{length}")
-			urlretrieve(f"https://{str(image['href'].split('//')[-1])}", f"{imgDir}/{threadMedia}")
+			print(f"Grabbing: {thread_media}")
+			url = f"https://{href_element.split('//')[-1]}"
+			directory_path = f"{img_dir}/{thread_media}"
+			await grab_media(url, directory_path)
 		except Exception as e:
 			print(e)
+
+def attempt_to_open_url():
+	try:
+		html = urlopen(sys.argv[1])
+		return html
+	except Exception as e:
+		print(e)
+
+async def main():
+	html = attempt_to_open_url()
+	bs4 = BeautifulSoup(html.read(), 'html.parser') 
+	post_image = bs4.findAll("a", {"class":"fileThumb"})
+	length = len(post_image)
+	img_dir = sys.argv[1].split('/')[-1]
+
+	print(f"making dir {img_dir}/ and checking {length} replies...")
+	try:
+		os.mkdir(img_dir)
+	except FileExistsError:
+		print(f"Folder already exists for this thread.")
+		update_from_thread = True
+
+	A, B = post_image[:length//2], post_image[length//2:]
+	task_1 = asyncio.create_task(parse_media_links(A, img_dir))
+	task_2 = asyncio.create_task(parse_media_links(B, img_dir))
+	await asyncio.gather(task_1, task_2)
+asyncio.run(main())
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 4chanRepliesMediaScraper
 Scrapes the media found within the replies to a 4chan thread and places them within a folder named after the thread number.
 
+`python3 4chanImageRipper.py <thread URL>`
+
 ## Requirements
-You'll need [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup) in order for this to work. 
+`pip3 install -r requirements.txt`
